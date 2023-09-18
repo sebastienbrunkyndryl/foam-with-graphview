3fcb56bc3281d223a6a401df0da2d2ad06ef2f6d
Sebastien Brun <sebastien.brun@kyndryl.com>
2023-09-18 16:26:38 +0200

    Configure for github backend

--- _layouts/gatsby-config.js
+++ _layouts/gatsby-config.js
@@ -3,12 +3,12 @@ const path = require('path')
 const PATH_PREFIX = process.env.PATH_PREFIX
 
 module.exports = {
-  // pathPrefix: PATH_PREFIX || `/foam-template-gatsby-kb`, // a. If you are using github pages, this should be the name of your repo
-  pathPrefix: PATH_PREFIX || `/`, // b. If you are using Netlify/Vercel, your can keep it this way
+  pathPrefix: PATH_PREFIX || `/foam-template-gatsby-kb`, // a. If you are using github pages, this should be the name of your repo
+  // pathPrefix: PATH_PREFIX || `/`, // b. If you are using Netlify/Vercel, your can keep it this way
   siteMetadata: {
     // some SEO configs using by gatsby-theme-kb
-    title: `Foam`, // Replace it with your site's title
-    author: `Your Name`, // Replace it with your name
+    title: `Foam KM with graphview test`, // Replace it with your site's title
+    author: `Sebastien Brun`, // Replace it with your name
     description: `My personal knowledge base`, // Replace it with your site's description
   },
   plugins: [
